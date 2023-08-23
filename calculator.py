051bd1b098bba7a7a2d77f01962bd8ca59226f9d
Anish Bhowmick <84652516+anishbhowmick@users.noreply.github.com>
2023-08-23 10:16:52 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+a=int(input('Enter your first number: '))
+b=int(input('Enter your second number: '))
+#add
+ans1 = a+b
+print("Addition of",a,"and",b,"is", ans1)
+#substraction
+ans2 = a-b
+print("Subtraction of",a,"and",b,"is", ans2)
+#multiplication
+ans3 = a*b
+print("Multiplication of",a,"and",b,"is", ans3)
+#division
+ans4 = a/b
+print("Division of",a,"and",b,"is", ans4)
+#modulus
+ans5 = a%b
+print("Modulus of",a,"and",b,"is", ans5)
+#floor division
+ans6 = a//b
+print("Floor Division of",a,"and",b,"is", ans6)
